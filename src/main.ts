89c0dd21425258b16eb0a130cce89a361ce51398
ValentinZoia <valentinzoia@gmail.com>
2025-09-16 22:08:44 -0300

    feat(add the initial configuration to handle dtos with class-validator)

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,19 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import morgan from 'morgan';
 import { CORS } from './constants';
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   //logs con morgan
   app.use(morgan('dev'));
+
+  //configuracion inicial para manejar dto con class validator
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
 
   const configService = app.get(ConfigService);
 
