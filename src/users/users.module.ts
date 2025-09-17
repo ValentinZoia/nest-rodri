22bee7dc7af2939b3c0ffdf1e55096b54cd07827
ValentinZoia <valentinzoia@gmail.com>
2025-09-17 14:13:19 -0300

    feat(create the auth module)

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,10 +5,18 @@ import { UsersController } from './controllers/users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/users.entity';
 import { UsersProjectsEntity } from './entities/usersProjects.entity';
+import { BcryptAdapter } from 'src/utils/bcrypt.adapter';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity, UsersProjectsEntity])], //features de typeorm modules
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [
+    UsersService,
+    {
+      provide: 'HashFunction',
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      useValue: BcryptAdapter.hash,
+    },
+  ],
 })
 export class UsersModule {}
