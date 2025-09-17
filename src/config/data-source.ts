af3f75493969be6c6986b48a86a8d622cba41e85
ValentinZoia <valentinzoia@gmail.com>
2025-09-16 22:05:58 -0300

    feat(create new mutations)

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,6 +1,8 @@
-import 'reflect-metadata';
+// import 'reflect-metadata';
 import { config } from 'dotenv';
 import { DataSource, DataSourceOptions } from 'typeorm';
+// import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import * as path from 'path';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 // Cargar variables de entorno
@@ -14,14 +16,44 @@ export const DataSourceConfig: DataSourceOptions = {
   username: process.env.DB_USER || 'valenzo',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'valenzodb',
-  entities: [__dirname + '../**/**/*.entity{.ts,.js}'],
-  migrations: [__dirname + '../../migrations/*{.ts,.js}'],
-  synchronize: true,
+  entities: [path.join(__dirname + '/../**/**/*.entity{.ts,.js}')],
+  migrations: [path.join(__dirname + '/../migrations/*{.ts,.js}')],
+  synchronize: false,
   migrationsRun: true,
-  // logging: false,
+  logging: false,
   namingStrategy: new SnakeNamingStrategy(),
 };
-
+console.log(DataSourceConfig.migrations);
 const AppDataSource = new DataSource(DataSourceConfig);
 
 export default AppDataSource;
+
+// import { ConfigModule, ConfigService } from '@nestjs/config';
+// import { DataSource, DataSourceOptions } from 'typeorm';
+// import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+// ConfigModule.forRoot({
+//   envFilePath: `.${process.env.NODE_ENV}.env`,
+// });
+
+// const configService = new ConfigService();
+
+// export const DataSourceConfig: DataSourceOptions = {
+//   type: 'postgres',
+//   host: configService.get('DB_HOST'),
+//   port: configService.get('DB_PORT'),
+//   username: configService.get('DB_USER'),
+//   password: configService.get('DB_PASSWORD'),
+//   database: configService.get('DB_NAME'),
+//   entities: [__dirname + '/../**/**/*.entity{.ts,.js}'],
+//   migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+//   synchronize: true,
+//   migrationsRun: true,
+//   logging: false,
+//   namingStrategy: new SnakeNamingStrategy(),
+// };
+// console.log(DataSourceConfig.entities);
+// console.log(DataSourceConfig.migrations);
+// console.log(DataSourceConfig.host);
+// console.log(DataSourceConfig.username);
+// export const AppDS = new DataSource(DataSourceConfig);
---
+++ src/migrations/1758050179043-init.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Init1758050179043 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
